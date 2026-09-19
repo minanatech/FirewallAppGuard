1a5491bfd3f786f6120064319ea825689c7cc50b
Minana Tech <minanatech@gmail.com>
2026-09-20 01:08:32 +0700

    Add AssemblyInfo for Firewall App Guard
    Added assembly information for the Firewall App Guard project.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Firewall App Guard")]
+[assembly: AssemblyDescription("Block apps from the internet using Windows Firewall")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("minanatech.com")]
+[assembly: AssemblyProduct("FirewallAppGuard")]
+[assembly: AssemblyCopyright("Copyright © minanatech.com 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("edd0325b-7bfe-44d4-a9ec-ee5e1eaf9bf5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
